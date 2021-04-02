b4f7b700b820b20dbbb641d6afb1b36426b039d7
Machanguele <josemachanguele@gmail.com>
2021-04-02 12:29:07 +0200

    Added: API Layer

---
+++ unico-api/API/Startup.cs
@@ -0,0 +1,87 @@
+using API.Extensions;
+using API.Middleware;
+using Application.Questions;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+      
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("SQLServer"))
+            );
+            
+            services.AddApplicationServices();
+            
+            services.AddIdentityServices(_configuration);
+            
+            services.AddSwaggerDocumentation();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                });
+            });
+
+            
+            
+            //services.AddMediatR(typeof(CreateUser.CreateUserCommand).Assembly);
+            
+            //services.AddControllers();
+            services.AddControllers()
+                
+                .AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<CreateQuestion>())
+                .AddNewtonsoftJson(options =>
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            
+            // services.AddAutoMapper(typeof(MappingProfiles));
+           
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
+            // app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            //app.UseAuthentication();
+
+            //app.UseAuthorization();
+
+            app.UseSwaggerDocumentation();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
